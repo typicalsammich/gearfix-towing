7b31c5d87fbecff619b0c173ba9269316fd0e3c5
typicalsammich <pansean777@gmail.com>
2026-09-15 13:28:34 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+GEAR FIXX WEBSITE
+
+Upload this folder to GitHub/Vercel. Before launch, replace {DOMAIN} in sitemap.xml with the final https:// domain. The site uses the primary phone number visible in the supplied logo: (780) 994-9013.
---
+++ script.js
@@ -0,0 +1,1 @@
+const m=document.querySelector('.menu'),n=document.querySelector('nav');if(m)m.onclick=()=>n.classList.toggle('open');document.querySelectorAll('nav a').forEach(a=>a.onclick=()=>n.classList.remove('open'));const c=document.querySelector('.mobilecall');addEventListener('scroll',()=>c&&c.classList.toggle('show',scrollY>140));
